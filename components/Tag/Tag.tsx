7f463ed0a94b6adbe561765a5f8ebfaa3574f5e8
YuichiOta <rb19780519@gmail.com>
2023-07-09 15:49:36 +0900

    Tag.tsx css修正

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -11,13 +11,13 @@ const Tag = (props: Props) => {
     <div className="mx-4">
       <section className="container lg:w-1/2 mb-8 mx-auto bg-gray-200 rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 duration-300 transition-all">
         <div className="font-medium mb-4 text-center">タグ検索</div>
-        <div className="flex:md justify-between flex-wrap">
+        <div className="flex justify-evenly items-center flex-wrap">
           {tags.map((tag: string, index: number ) => (
-            <Link href={`/posts/tag/${tag}/page/1`} key={index}>
-              <span className="cursor-pointer mb-5 px-2 pb-2 rounded-xl bg-gray-500 text-white inline-block w-1/2 md:w-1/3 text-xs md:text-base text-center">
-                {tag}
-              </span>
-            </Link>
+            <span className="cursor-pointer mb-5 px-2 pb-2 py-2 rounded-xl bg-gray-500 text-white inline-block w-2/5 text-xs md:text-base text-center">
+              <Link href={`/posts/tag/${tag}/page/1`} key={index}>
+                  {tag}
+              </Link>
+            </span>
           ))}
         </div>
       </section>
